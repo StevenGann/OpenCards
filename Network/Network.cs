ce15944a2bbe5d9a1cf52b51aa45cef2128edd3d
Jacen47 <jcnhill@gmail.com>
2014-08-23 17:48:07 -0700

    Network Initial Update
    Included libraries for both Client and Server Netcode.

--- Network/Network.cs
+++ Network/Network.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets.TcpClient;
+using System.Net.Sockets.TcpListener;
 
 namespace Network
 {
