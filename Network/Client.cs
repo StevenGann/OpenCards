826d75b18e808baf5318b5b33850bf74584d0fe0
Jacen47 <jcnhill@gmail.com>
2015-01-23 20:52:58 -0500

    Netcode strip down for re-write
    Removal of code that is no longer necessary before near-complete
    re-write.

--- Network/Client.cs
+++ Network/Client.cs
@@ -15,8 +15,6 @@ namespace Network
         private List<Object> Messages = new List<Object>();
         private TcpClient Server;
         private Thread ListenerThread;
-        private Boolean ConnectionState;
-        private Boolean Pinging = false;
 
         // Constructor to start the message listener in its own thread
         public Client(IPAddress Address, int Port)
@@ -51,57 +49,18 @@ namespace Network
                 {
                     break; // Client disconnection
                 }
-                else if (BytesRead == 4)
-                {
-                    if (Pinging)
-                    {
-                        ConnectionState = true;
-                    }
-                    else
-                    {
-                        Ping();
-                    }
-                }
-
-                DeserializeMessage(Message);
             }
-            ConnectionState = false;
             Server.Close();
         }
 
         // Send a message to the given client
         public void sendMessage(Object Message)
         {
-            NetworkStream NetStream = new NetworkStream(Server.Client);
-
-            BinaryFormatter BinForm = new BinaryFormatter();
-            MemoryStream MemStream = new MemoryStream();
-            BinForm.Serialize(MemStream, Message);
-            byte[] OutMessageObj = MemStream.ToArray();
-            byte[] OutMessageLength = BitConverter.GetBytes(OutMessageObj.Length);
-            byte[] OutMessage = new byte[OutMessageObj.Length + OutMessageLength.Length];
-            OutMessageLength.CopyTo(OutMessage, 0);
-            OutMessageObj.CopyTo(OutMessage, OutMessageLength.Length);
-            NetStream.Write(OutMessage, 0, OutMessage.Length);
         }
 
         // Deserialize a message
         private void DeserializeMessage(Byte[] Message)
         {
-            int MessageLength = BitConverter.ToInt32(Message, 0);
-
-            if (MessageLength == -1)
-            {
-                ConnectionState = true;
-                return;
-            }
-
-            //Deserialize message
-            MemoryStream MemStream = new MemoryStream();
-            BinaryFormatter BinForm = new BinaryFormatter();
-            MemStream.Write(Message, 4, MessageLength);
-            Object MessageObj = (Object)BinForm.Deserialize(MemStream);
-            Messages.Add(MessageObj);
         }
 
         // Get the most recent message
@@ -117,31 +76,6 @@ namespace Network
             {
                 return null;
             }
-        }
-
-        //Ping the server
-        private Boolean Ping()
-        {
-            NetworkStream NetStream = new NetworkStream(Server.Client);
-            Pinging = true;
-
-            byte[] Ping = BitConverter.GetBytes(-1);
-            NetStream.Write(Ping, 0, Ping.Length);
-            Thread.Sleep(300);
-
-            Pinging = false;
-            return IsConnected();
-        }
-
-        public Boolean IsPinging()
-        {
-            return Pinging;
-        }
-
-        // Check connection status
-        public Boolean IsConnected()
-        {
-            return ConnectionState;
         }
     }
 }
--- Network/Server.cs
+++ Network/Server.cs
@@ -17,7 +17,6 @@ namespace Network
         private List<Object> Messages = new List<Object>();
         private List<TcpClient> Clients = new List<TcpClient>();
         private List<TcpClient> LiveClients = new List<TcpClient>();
-        private Boolean Pinging = false;
 
         //Simple constructor that creates a listener then assigns that listener to a thread
         public Server(IPAddress Address, int Port)
@@ -67,19 +66,6 @@ namespace Network
                 {
                     break; // Client disconnection
                 }
-                else if (BytesRead == 4)
-                {
-                    if(Pinging)
-                    {
-                        LiveClients.Add(Client);
-                    }
-                    else
-                    {
-                        Ping((TcpClient)client);
-                    }
-                }
-
-                DeserializeMessage(Message);
             }
 
             Client.Close();
@@ -88,17 +74,6 @@ namespace Network
         // Send a message to the given client
         public void sendMessage(Object Message, TcpClient Client)
         {
-            NetworkStream NetStream = new NetworkStream(Client.Client);
-            
-            BinaryFormatter BinForm = new BinaryFormatter();
-            MemoryStream MemStream = new MemoryStream();
-            BinForm.Serialize(MemStream, Message);
-            byte[] OutMessageObj = MemStream.ToArray();
-            byte[] OutMessageLength = BitConverter.GetBytes(OutMessageObj.Length);
-            byte[] OutMessage = new byte[OutMessageObj.Length + OutMessageLength.Length];
-            OutMessageLength.CopyTo(OutMessage, 0);
-            OutMessageObj.CopyTo(OutMessage, OutMessageLength.Length);
-            NetStream.Write(OutMessage, 0, OutMessage.Length);
         }
 
         // Get the most recent message
@@ -132,53 +107,9 @@ namespace Network
             }
         }
 
-        // Check if all players are still connected and remove them if not
-        public List<TcpClient> IsConnected(List<TcpClient> Clients)
-        {
-            Pinging = true;
-            for (int i = 0; i <= Clients.Count; i++)
-            {
-                Ping(Clients[i]);
-            }
-            Thread.Sleep(350);
-            Pinging = false;
-            return LiveClients;
-        }
-
-        //Ping a client
-        private void Ping(TcpClient Client)
-        {
-            NetworkStream NetStream = new NetworkStream(Client.Client);
-
-            byte[] Ping = BitConverter.GetBytes(-1);
-            NetStream.Write(Ping, 0, Ping.Length);
-        }
-
-        public Boolean IsPinging()
-        {
-            return Pinging;
-        }
-
         // Deserialize a message
         private void DeserializeMessage(Byte[] Message)
         {
-            int MessageLength = BitConverter.ToInt32(Message, 0);
-
-            if (MessageLength == -1)
-            {
-                return;
-            }
-            else // This shouldn't happen and is just error catching
-            {
-                LiveClients.RemoveAt(0);
-            }
-
-            //Deserialize message
-            MemoryStream MemStream = new MemoryStream();
-            BinaryFormatter BinForm = new BinaryFormatter();
-            MemStream.Write(Message, 4, MessageLength);
-            Object MessageObj = (Object)BinForm.Deserialize(MemStream);
-            Messages.Add(MessageObj);
         }
     }
 }
