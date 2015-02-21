4b2c5cdc5392e400e3e61829115278fd4bccfbc9
Jacen47 <jcnhill@gmail.com>
2015-02-21 16:35:03 -0500

    Message Class Skeleton

---
+++ Network/Message.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Network
+{
+    class Message
+    {
+        private string    objectType;
+        private object    message;
+        private string    timeSent;
+        private string    timeReceived;
+        public  TcpClient recipient;
+        public  int       ping;
+
+
+        public Message()
+        { }
+
+        public Message(TcpClient recip, string type, object msg)
+        {
+            recipient = recip;
+            objectType = type;
+            message = msg;
+        }
+
+        public void setObjectType(string type)    { objectType = type; }
+        public void setMessage(object msg)        { message = msg; }
+        //public void setTimeSent();
+    }
+}
